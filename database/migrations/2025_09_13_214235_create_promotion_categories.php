4a5b12d2a2df482239d98be077250dca8a7c6beb
Hadi Zorkot <hzorkot075@gmail.com>
2025-09-14 00:43:40 +0300

    update promotion categories

--- database/migrations/2025_09_13_214235_create_promotion_categories.php
+++ database/migrations/2025_09_13_214235_create_promotion_categories.php
@@ -13,8 +13,12 @@ return new class extends Migration
     {
         Schema::create('promotion_categories', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('promotion_id')->constrained('promotions');
-            $table->foreignId('category_id')->constrained('product_categories');
+            $table->foreignId('promotion_id')
+                  ->constrained('promotions')  
+                  ->onDelete('cascade');  
+            $table->foreignId('category_id')
+                  ->constrained('product_categories')  
+                  ->onDelete('cascade'); 
             $table->timestamps();
         });
     }
